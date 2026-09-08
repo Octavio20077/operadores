db2ead4e4cf2c86cd4d273e9edc74a6afc4f08b6
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-08 10:40:34 -0300

    Primer commit_operadoresAritmeticos

---
+++ src/operadoresAritmeticos.java
@@ -0,0 +1,23 @@
+public class operadoresAritmeticos {
+    public static void main(String[] args) {
+
+        System.out.println("***Operadores Aritmeticos***");
+
+        int a = 5, b = 3, resultado;
+        //suma
+        resultado = a + b;
+        System.out.println("Resultado de la suma: " + resultado);
+        //resta
+        resultado = a - b;
+        System.out.println("Resultado de la resta: " + resultado);
+        //multiplicacion
+        resultado = a * b;
+        System.out.println("Resultado de la multiplicacion: " + resultado);
+        //division
+        resultado = a / b;
+        System.out.println("Resutado de la division: " + resultado);
+        //modulo(residuo de division)
+        resultado = a % b;
+        System.out.println("El modulo es: " + resultado);
+    }
+}
