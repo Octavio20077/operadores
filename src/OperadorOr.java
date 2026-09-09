165f4a414671ba8c958cc46dd072c9e341a087f3
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-09 10:33:28 -0300

    agregado el archivo Operador Or

---
+++ src/OperadorOr.java
@@ -0,0 +1,8 @@
+public class OperadorOr {
+    public static void main(String[] args) {
+        System.out.println("***Operador Or***");
+        boolean a = true, b = false;
+        var reultado = a || b;
+        System.out.println("reultado = " + reultado);
+    }
+}
