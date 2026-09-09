b3c0621b821bd0105b5cec2592e7f315277c8e41
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-09 10:23:21 -0300

    agregado el archivo Operdor And

---
+++ src/OperadorAnd.java
@@ -0,0 +1,9 @@
+public class OperadorAnd {
+    public static void main(String[] args) {
+        System.out.println("***Operador And***");
+
+        boolean a = true, b = false;
+        var resultado = a && b;
+        System.out.println("resultado = " + resultado);
+    }
+}
