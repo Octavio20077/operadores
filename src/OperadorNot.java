d043b6dcff950f32e10994ea18ca4249f1941cf2
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-09 10:37:26 -0300

    agregado el archivo Operador Not

---
+++ src/OperadorNot.java
@@ -0,0 +1,8 @@
+public class OperadorNot {
+    public static void main(String[] args) {
+        System.out.println("***Operador Not***");
+        boolean a = true;
+        var resultado = !a;
+        System.out.println("resultado = " + resultado);
+    }
+}
