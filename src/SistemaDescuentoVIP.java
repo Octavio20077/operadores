1c0dd51034bd2eba06525dd98b4a0a7e9c98c02f
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-09 11:46:34 -0300

    Agregado el archivo Sistema de descuento VIP

---
+++ src/SistemaDescuentoVIP.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SistemaDescuentoVIP {
+    public static void main(String[] args) {
+        System.out.println("***Sistema de descueto VIP***");
+         var cantidadProductodescuento = 10;
+         var consola = new Scanner(System.in);
+
+        System.out.print("--Cuantos productos compraste hoy?: ");
+        var cantProductos = Integer.parseInt(consola.nextLine());
+
+        System.out.print("--Tienes la membresia?:");
+        var Membresia = Boolean.parseBoolean(consola.nextLine());
+
+        var eselegibleEldescuento = cantProductos >= cantidadProductodescuento && Membresia;
+        System.out.print("eselegibleEldescuento = " + eselegibleEldescuento);
+
+
+    }
+}
