ab81c223fad920d8a8cacbd4a0798753758b69f2
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-09 21:04:03 -0300

    Agregado el archivo Presedencia de operdores

---
+++ src/PresedenciadeOperadores.java
@@ -0,0 +1,21 @@
+public class PresedenciadeOperadores {
+    public static void main(String[] args) {
+        // Precedencia de Operadores en Java:
+// 1. Parentesis y Corchetes
+// 2. Operadores unarios -, ++, --
+// 3. Operadores aritmeticos *, / y %
+// 4. Operadores aritmeticos + y -
+// 5. Relacionales <, <=, >, >=
+// 6. Equivalencia == y !=
+// 7. Logicos && y ||
+// 8. Asignacion =, +=, -=, *=, etc
+
+        //Expresion
+        //paso 1. Division 12/3 = 4
+        //paso 2. Multiplicacion 2 * 3 = 6
+        //paso 3. suma 6 + 4 = 10
+        //paso 4. resta 10 - 1 = 9 
+        int a = 12 / 3 + 2 * 3 - 1;
+        System.out.println("a = " + a);
+    }
+}
